636500ffd8fededb45384e09deca94093188d251
박현재 <hyeonjae@users.noreply.github.com>
2018-02-13 10:49:45 +0900

    Update toy.cpp

--- chapter2/toy.cpp
+++ chapter2/toy.cpp
@@ -3,7 +3,6 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Support/raw_ostream.h"
 #include <cctype>
 #include <cstdio>
 #include <map>
